f5400347cd198965020ba363abca7fc2322613f6
Krzysztof Rusek <kr292291@students.mimuw.edu.pl>
2015-02-09 19:09:57 +0100

    setup.py script

---
+++ scripts/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$( dirname "$0" )/.."
+
+rm -Rf build dist htmlcov yorbay.egg-info .coverage
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yorbay',
+    version='0.1.dev1',
+    author='Krzysztof Rusek',
+    author_email='savix5@gmail.com',
+    description='Python implementation of l20n file format',
+    url='https://github.com/rusek/yorbay-python',
+    license='Apache License, Version 2.0',
+    keywords='localization',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Internationalization',
+    ],
+)
