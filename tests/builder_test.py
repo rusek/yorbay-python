f909ce7840c2d3bc6082cf27cf563b4c3aa5126f
Krzysztof Rusek <kr292291@students.mimuw.edu.pl>
2015-02-21 14:04:09 +0100

    Maintain builder cache integrity

--- tests/builder_test.py
+++ tests/builder_test.py
@@ -10,7 +10,21 @@ sys.path[0] = os.path.dirname(DIR)
 
 from yorbay.builder import build_from_path, build_from_standalone_source
 from yorbay.compiler import CircularDependencyError
-from yorbay.loader import FsLoader
+from yorbay.loader import FsLoader, SimpleLoader, LoaderError
+
+
+class DictLoader(SimpleLoader):
+    def __init__(self, files):
+        super(DictLoader, self).__init__()
+        self.files = files
+        self.load_count = 0
+
+    def load_source(self, path):
+        try:
+            self.load_count += 1
+            return self.files[path]
+        except KeyError:
+            raise LoaderError('Not found: {0}'.format(path))
 
 
 class TestBuildFromStandaloneSource(unittest.TestCase):
@@ -31,6 +45,38 @@ class TestCircularDependencies(unittest.TestCase):
 
     def test_cycle_of_several_modules(self):
         self.assertRaises(CircularDependencyError, build_from_path, 'a-depends-on-b.l20n', loader=self.loader)
+
+
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        self.files = {
+            'main.l20n': 'import("first.l20n") import("second.l20n") import("third.l20n") '
+                         '<main "{{ first }} {{ second }} {{ third }}">',
+            'first.l20n': '<first "first">',
+            'second.l20n': '<second "second">',
+            'third.l20n': '<third "third">',
+        }
+        self.loader = DictLoader(self.files)
+        self.cache = {}
+
+    def build(self):
+        return build_from_path('main.l20n', loader=self.loader, cache=self.cache)
+
+    def build_success(self):
+        env = self.build().make_env()
+        self.assertEqual(env.resolve_entity('main'), 'first second third')
+
+    def test_served_from_cache(self):
+        self.build_success()
+        self.assertEqual(self.loader.load_count, 4)
+        self.build_success()
+        self.assertEqual(self.loader.load_count, 4)
+
+    def test_cache_integrity(self):
+        second_data = self.files.pop('second.l20n')
+        self.assertRaises(LoaderError, self.build)
+        self.files['second.l20n'] = second_data
+        self.build_success()
 
 
 if __name__ == '__main__':
--- yorbay/builder.py
+++ yorbay/builder.py
@@ -9,7 +9,8 @@ class BuilderError(BuildError):
 
 
 class Goal(object):
-    def __init__(self):
+    def __init__(self, path):
+        self.path = path
         self.cstate = None
         self.import_goals = None
         self.out_import_cstates = None
@@ -21,9 +22,9 @@ class Builder(object):
             loader = FsLoader()
 
         self._loader = loader
-        self._all = []
         self._unprocessed = []
-        self._cache = {} if cache is None else cache
+        self._cstate_cache = cache
+        self._goal_cache = {}
         self._debug = debug
 
     def get_goal(self, path):
@@ -31,18 +32,22 @@ class Builder(object):
 
     def _get_goal(self, path):
         try:
-            return self._cache[path]
+            return self._goal_cache[path]
         except KeyError:
-            goal = Goal()
-            self._cache[path] = goal
-            self._unprocessed.append((goal, None, path))
-            self._all.append(goal)
+            goal = Goal(path)
+            self._goal_cache[path] = goal
+            if self._cstate_cache is not None:
+                try:
+                    goal.cstate = self._cstate_cache[path]
+                    return goal
+                except KeyError:
+                    pass
+            self._unprocessed.append((goal, None))
             return goal
 
     def get_anonymous_goal(self, source, path):
-        goal = Goal()
-        self._unprocessed.append((goal, source, self._loader.prepare_path(path)))
-        self._all.append(goal)
+        goal = Goal(self._loader.prepare_path(path))
+        self._unprocessed.append((goal, source))
         return goal
 
     def __enter__(self):
@@ -53,21 +58,26 @@ class Builder(object):
             self.run()
 
     def run(self):
-        while self._unprocessed:
-            self._process(*self._unprocessed.pop())
+        i = 0
+        while i < len(self._unprocessed):
+            self._process(*self._unprocessed[i])  # may introduce new unprocessed goals
+            i += 1
 
-        for goal in self._all:
+        for goal, _ in self._unprocessed:
             goal.out_import_cstates[:] = [igoal.cstate for igoal in goal.import_goals]
+            if self._cstate_cache is not None:
+                self._cstate_cache[goal.path] = goal.cstate
 
-    def _process(self, goal, source, path):
+    def _process(self, goal, source):
         if source is None:
-            source = self._loader.load_source(path)
+            source = self._loader.load_source(goal.path)
 
         goal.cstate, import_paths, goal.out_import_cstates = compile_syntax(
-            parse_source(source, path=self._loader.format_path(path), debug=self._debug),
+            parse_source(source, path=self._loader.format_path(goal.path), debug=self._debug),
             debug=self._debug
         )
-        goal.import_goals = [self._get_goal(self._loader.prepare_import_path(path, ipath)) for ipath in import_paths]
+        goal.import_goals = [self._get_goal(self._loader.prepare_import_path(goal.path, ipath))
+                             for ipath in import_paths]
 
 
 def build_from_source(source, path='', loader=None, cache=None, debug=False):
